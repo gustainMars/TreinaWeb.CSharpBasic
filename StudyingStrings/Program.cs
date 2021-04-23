0351b41d0623de67d466c11c12cd573a1e11e899
gustasDesktop <gustaDesktop@github.com>
2021-04-22 23:00:14 -0300

    Studying Strings and StringBuilder
    Learned StringBuilder Append method.

---
+++ StudyingStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace StudyingStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "Gustavo";
+            String lastName = "Marques";
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(name);
+            sb.Append(" ");
+            sb.Append(lastName);
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
